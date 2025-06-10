b36a76a65eb9a009ee4bfd0f45520ea40a07a059
rafasanzvi <rafasv41@gmail.com>
2025-06-10 12:20:41 +0200

    primera subida ejercicio

---
+++ bikram_1.js
@@ -0,0 +1,54 @@
+// ejercicio 1
+let variableSinValor;
+
+//ejercicio 2
+let booleano1 = false;
+let booleano2 = true;
+
+//ejercicio 3
+const PI = 3.14;
+
+//ejercicio 4
+const TAU = 2 * PI;
+
+//ejercicio 5
+let booleanoAnd = booleano1 && booleano2
+
+//ejercicio 6
+let booleanoNot = !booleano1;
+
+//ejercicio 7
+let booleanoMix0 = booleano1 + booleano2 && booleano1
+
+//ejercicio 8
+let incrementarDesp = "2";
+let resultadoDesp = incrementarDesp++
+
+//ejercicio 9
+let incrementarAntes = "2";
+let resultadoAntes = ++incrementarAntes;
+
+//ejercicio 10
+let contarHasta10_2 = 0;
+for (let i= 0; i < 10; i++) {
+contarHasta10_2++;
+}
+console.log (contarHasta10_2)
+
+//ejercicio 11
+let postI = 0;
+let postJ = 0;
+for (i = 0; i < 11; i++) {
+    postI = postI + postJ++;
+}
+
+//ejercicio 12
+// let sumaPares = 0
+// for (i = 0; i < 10; i++) {
+// 
+
+//ejercicio 13
+
+
+//ejercicio 17
+//let booleanoMix1 = (booleano1 && (TAU / 2 === PI)) || (variableValorNumerico >= MiNumeroFav);
