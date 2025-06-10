9b20c484a234a6960ad1f6a0d011005ca5ba6075
rafasanzvi <rafasv41@gmail.com>
2025-06-10 16:19:13 +0200

    ejercicios acabados

---
+++ index.js
@@ -0,0 +1,105 @@
+// ejercicio 1
+let variableSinValor;
+
+//ejercicio 2
+let booleano1 = false;
+let booleano2 = true;
+
+//ejercicio 3
+const PI = 3.14;
+
+//ejercicio 4
+const TAU = 2 * PI;
+
+//ejercicio 5
+let booleanoAnd = booleano1 && booleano2
+
+//ejercicio 6
+let booleanoNot = !booleano1;
+
+//ejercicio 7
+let booleanoMix0 = booleano1 + booleano2 && booleano1
+
+//ejercicio 8
+let incrementarDesp = "2";
+let resultadoDesp = incrementarDesp++
+
+//ejercicio 9
+let incrementarAntes = "2";
+let resultadoAntes = ++incrementarAntes;
+
+//ejercicio 10
+let contarHasta10_2 = 0;
+for (let i = 0; i < 10; i++) {
+    contarHasta10_2++;
+}
+//console.log(contarHasta10_2)
+
+//ejercicio 11
+let postI = 0;
+let postJ = 0;
+for (i = 0; i < 11; i++) {
+    postI = postI + postJ++;
+}
+
+//ejercicio 12
+let sumaPares = 0;
+for (i = 0; i < 10; i++) {
+    if (i % 2 == 0) {
+        sumaPares += i;
+    }
+}
+
+//ejercicio 13
+let variableValorNumerico = 2;
+
+//ejercicio 14
+const MiNombre = "Nazaret";
+
+//ejercicio 15
+const MiNumeroFav = 16;
+
+//ejercicio 16
+let booleanoOr = booleano1 || booleano2;
+
+//ejercicio 17
+let booleanoMix1 = (booleano1 && (TAU / 2 === PI)) || (variableValorNumerico >= MiNumeroFav);
+
+//ejercicio 18
+let seisNoEsNueve = booleano1 !== booleano2;
+
+//ejercicio 19 preguntar x este
+let booleanoMix2 = (variableValorNumerico < 16) || (-(MiNumeroFav * TAU));
+
+//ejercicio 20
+let valorSuma = MiNumeroFav + variableValorNumerico;
+
+//ejercicio 21
+let valorResta = MiNumeroFav - variableValorNumerico;
+
+//ejercicio 22
+let valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+
+//ejercicio 23
+let valorDivision = MiNumeroFav / 3;
+
+//ejercicio 24 PREGUNTAR
+let contarHasta10 = 0;
+while (contarHasta10 < 10) {
+    contarHasta10++;
+}
+
+//ejercicio 25
+let preI = 0;
+let preJ = 0;
+for (i = 0; i < 11; i++) {
+    preI += ++preJ
+}
+
+//ejercicio 26 PREGUNTAR
+let sumaImpares = 0;
+for (i = 0; i < 10; i++) {
+    if (i % 2 !== 0) {
+        sumaImpares += +i;
+    }
+}
